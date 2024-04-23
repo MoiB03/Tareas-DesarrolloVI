9952c05911c31150bb40c28ce8f335328d1488ae
yeafer01 <144929715+yeafer01@users.noreply.github.com>
2024-04-23 18:02:54 -0500

    parte 2
    Parte 2 Terminada

--- Demo_UI_exa_1/app/src/main/java/com/example/demo_ui_exa_1/fragment_option2.kt
+++ Demo_UI_exa_1/app/src/main/java/com/example/demo_ui_exa_1/fragment_option2.kt
@@ -8,13 +8,13 @@ import android.view.View
 import android.view.ViewGroup
 import android.widget.Button
 import android.widget.EditText
+import android.widget.RadioGroup
 import android.widget.Toast
 
-
 class FragmentOption2 : Fragment() {
-    private lateinit var editTextUsername: EditText
-    private lateinit var editTextPassword: EditText
-    private lateinit var buttonLogin: Button
+
+    private lateinit var buttonver: Button
+    private lateinit var radioGroup: RadioGroup
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -23,27 +23,20 @@ class FragmentOption2 : Fragment() {
         // Inflate the layout for this fragment
         val view = inflater.inflate(R.layout.fragment_option2, container, false)
 
-        editTextUsername = view.findViewById(R.id.editTextUsername)
-        editTextPassword = view.findViewById(R.id.editTextPassword)
-        buttonLogin = view.findViewById(R.id.buttonLogin)
 
-        buttonLogin.setOnClickListener {
-            val username = editTextUsername.text.toString()
-            val password = editTextPassword.text.toString()
+        radioGroup = view.findViewById(R.id.radioGroup)
 
-            // Verificar si el usuario y la contraseña son correctos
-            if (username == "usuario" && password == "12345") {
-                // Iniciar la actividad principal si son correctos
-                val intent = Intent(requireContext(), MainActivity::class.java)
-                startActivity(intent)
-                requireActivity().finish() // Finalizar la actividad actual para evitar volver a ella desde la actividad principal
-            } else {
-                // Mostrar un mensaje de error si son incorrectos
-                Toast.makeText(requireContext(), "Usuario o contraseña incorrectos", Toast.LENGTH_SHORT).show()
+        // Set the OnCheckedChangeListener to the RadioGroup
+        radioGroup.setOnCheckedChangeListener { group, checkedId ->
+            when (checkedId) {
+                R.id.radioButton1 -> Toast.makeText(requireContext(), "No es el correcto", Toast.LENGTH_SHORT).show()
+                R.id.radioButton2 -> Toast.makeText(requireContext(), "No es el correcto", Toast.LENGTH_SHORT).show()
+                R.id.radioButton3 -> Toast.makeText(requireContext(), "es el correcto", Toast.LENGTH_SHORT).show()
+                R.id.radioButton4 -> Toast.makeText(requireContext(), "No es el correcto", Toast.LENGTH_SHORT).show()
+                else -> Toast.makeText(requireContext(), "Elija una opcion", Toast.LENGTH_SHORT).show()
             }
         }
 
         return view
     }
-
 }
