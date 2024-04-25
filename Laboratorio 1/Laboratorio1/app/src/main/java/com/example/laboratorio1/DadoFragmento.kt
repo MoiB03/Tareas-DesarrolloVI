73fea1d466f2f4bceba57328d6b1444bd737cdd7
MoiB03 <145634708+MoiB03@users.noreply.github.com>
2024-04-25 11:18:54 -0500

    hola

--- Laboratorio 1/Laboratorio1/app/src/main/java/com/example/laboratorio1/DadoFragmento.kt
+++ Laboratorio 1/Laboratorio1/app/src/main/java/com/example/laboratorio1/DadoFragmento.kt
@@ -6,9 +6,11 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Button
+import android.widget.ImageView
 
 class DadoFragmento : Fragment() {
     private lateinit var button: Button
+    private lateinit var imagen: ImageView
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -24,6 +26,21 @@ class DadoFragmento : Fragment() {
 
         }
         return view
+        fun dadoAleatorio(numeroDado:String){
+            imagen = imagen.findViewById(R.id.dado)
+
+            when(numeroDado){
+                "1" ->     imagen.setImageResource(R.drawable.dado1)
+                "2" ->     imagen.setImageResource(R.drawable.dado2)
+                "3" ->     imagen.setImageResource(R.drawable.dado3)
+                "4" ->     imagen.setImageResource(R.drawable.dado4)
+                "5" ->     imagen.setImageResource(R.drawable.dado5)
+                "6" ->     imagen.setImageResource(R.drawable.dado6)
+
+
+            }
+
+        }
     }
 
 
