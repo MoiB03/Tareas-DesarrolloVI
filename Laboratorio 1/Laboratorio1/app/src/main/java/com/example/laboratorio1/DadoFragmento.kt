e07710ce445ea897b9dc5a1965f5e865aaf224f1
MoiB03 <145634708+MoiB03@users.noreply.github.com>
2024-05-01 17:10:09 -0500

    Finalizado

--- Laboratorio 1/Laboratorio1/app/src/main/java/com/example/laboratorio1/DadoFragmento.kt
+++ Laboratorio 1/Laboratorio1/app/src/main/java/com/example/laboratorio1/DadoFragmento.kt
@@ -5,9 +5,11 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.view.animation.*
 import android.widget.Button
 import android.widget.ImageView
 import android.widget.TextView
+import androidx.core.content.ContextCompat
 import kotlin.random.Random
 
 class DadoFragmento : Fragment() {
@@ -26,35 +28,65 @@ class DadoFragmento : Fragment() {
         imagen = view.findViewById(R.id.dado)
         tv =view.findViewById(R.id.textView)
         button.setOnClickListener {
-            val numeroAleatorio = generarNumeroAleatorio()
-            dadoAleatorio(numeroAleatorio.toString())
+
+            dadoAleatorio(rotarDado())
+
         }
         return view
     }
 
-    private fun dadoAleatorio(numeroDado: String) {
-        if (numeroDado == "6") {
-            view?.setBackgroundColor(resources.getColor(android.R.color.holo_green_dark))
+
+    private fun dadoAleatorio(numeroDado: Int) {
+        if (numeroDado == 6) {
+            view?.setBackgroundColor(ContextCompat.getColor(requireContext(), R.color.dorado))
             tv.text = "¡Ganaste!"
         } else {
-            view?.setBackgroundColor(resources.getColor(android.R.color.white))
+            view?.setBackgroundColor(ContextCompat.getColor(requireContext(), R.color.gris_claro))
             tv.text = "Sigue intentando"
         }
 
+    }
+
+    private fun rotarDado(): Int{
+        var numeroDado =  Random.nextInt(6) + 1
+
+        val rotateAnimation = RotateAnimation(
+            0f, 360f,
+            Animation.RELATIVE_TO_SELF, 0.5f,
+            Animation.RELATIVE_TO_SELF, 0.5f
+        )
+        rotateAnimation.duration = 500 // Duración de la animación en milisegundos
+        rotateAnimation.repeatCount = 0
+
         // Setear la imagen del dado
         when (numeroDado) {
-            "1" -> imagen.setImageResource(R.drawable.dado1)
-            "2" -> imagen.setImageResource(R.drawable.dado2)
-            "3" -> imagen.setImageResource(R.drawable.dado3)
-            "4" -> imagen.setImageResource(R.drawable.dado4)
-            "5" -> imagen.setImageResource(R.drawable.dado5)
-            "6" -> imagen.setImageResource(R.drawable.dado6)
+            1 -> {
+                imagen.setImageResource(R.drawable.dado1)
+                imagen.startAnimation(rotateAnimation)
+            }
+            2 -> {
+                imagen.setImageResource(R.drawable.dado2)
+                imagen.startAnimation(rotateAnimation)
+            }
+            3 -> {
+                imagen.setImageResource(R.drawable.dado3)
+                imagen.startAnimation(rotateAnimation)
+            }
+            4 -> {
+                imagen.setImageResource(R.drawable.dado4)
+                imagen.startAnimation(rotateAnimation)
+            }
+            5 -> {
+                imagen.setImageResource(R.drawable.dado5)
+                imagen.startAnimation(rotateAnimation)
+            }
+            6 -> {
+                imagen.setImageResource(R.drawable.dado6)
+                imagen.startAnimation(rotateAnimation)
+            }
         }
+        return numeroDado
     }
 
-
-
-    private fun generarNumeroAleatorio(): Int {
-        return Random.nextInt(6) + 1
-    }
 }
+
--- Laboratorio 1/Laboratorio1/app/src/main/java/com/example/laboratorio1/MainActivity.kt
+++ Laboratorio 1/Laboratorio1/app/src/main/java/com/example/laboratorio1/MainActivity.kt
@@ -4,11 +4,9 @@ import android.os.Bundle
 import android.view.View
 import android.widget.Button
 import android.widget.ImageView
-import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.appcompat.widget.Toolbar
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
+
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
