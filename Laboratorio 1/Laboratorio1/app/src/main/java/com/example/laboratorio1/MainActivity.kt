e0d1f3e20d76d2f733ac21a86e9fb4231dd41f46
MoiB03 <145634708+MoiB03@users.noreply.github.com>
2024-06-05 00:40:33 -0500

    Actualización DadoApp

--- Laboratorio 1/Laboratorio1/app/src/main/java/com/example/laboratorio1/MainActivity.kt
+++ Laboratorio 1/Laboratorio1/app/src/main/java/com/example/laboratorio1/MainActivity.kt
@@ -8,6 +8,7 @@ package com.example.laboratorio1
 import android.os.Bundle
 import android.view.View
 import android.widget.Button
+import android.widget.EditText
 import android.widget.ImageView
 import androidx.appcompat.app.AppCompatActivity
 import androidx.appcompat.widget.Toolbar
@@ -26,18 +27,20 @@ class MainActivity : AppCompatActivity() {
 
         val btninicio = findViewById<Button>(R.id.btnIrAJugar)
         val imgDado = findViewById<ImageView>(R.id.dado_feliz)
+        val usuario = findViewById<EditText>(R.id.usuario)
+        val password = findViewById<EditText>(R.id.password)
 
         btninicio.setOnClickListener {
             btninicio.visibility = View.GONE
             imgDado.visibility = View.GONE
+            toolbar.subtitle = "¡Prueba tu suerte!"
+            usuario.visibility = View.GONE
+            password.visibility = View.GONE
 
             supportFragmentManager.beginTransaction()
                 .add(R.id.fragment_container, dadoFragmento)//Agregar el Fragmento al contenedor
                 .commit()
         }
-
-
-
 
     }
 }
